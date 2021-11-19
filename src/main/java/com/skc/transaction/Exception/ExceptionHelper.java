e1bb7aa157baabed2d6953bc34c1c16b3b2ee1de
Rakesh Chaudhari <rakeshchaudhari1111@gmail.com>
2021-11-19 17:39:19 +0530

    Added handler code for NOT FOUND requests

--- src/main/java/com/skc/transaction/Exception/ExceptionHelper.java
+++ src/main/java/com/skc/transaction/Exception/ExceptionHelper.java
@@ -4,9 +4,12 @@ import javax.servlet.ServletException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.skc.transaction.model.ResponseResultBody;
 import com.skc.transaction.utils.Constant;
@@ -28,4 +31,11 @@ public class ExceptionHelper {
 		return new ResponseResultBody(Constant.FAILED, 500, Constant.EMPTY_RESULT, ex.getMessage());
 	}
 
+	@ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+	public @ResponseBody ResponseResultBody NoHandlerFoundException(ServletException ex) {
+		logger.error("HandleNoHandlerFound Exception: ", ex.getMessage());
+		return new ResponseResultBody(Constant.FAILED, HttpStatus.NOT_FOUND.value(), Constant.EMPTY_RESULT, ex.getMessage());
+	}
+	
 }
